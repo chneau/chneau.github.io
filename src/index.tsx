1fc928e29b9cf62777c2e271e69fc45f4501a315
chneau <charles63500@gmail.com>
2026-01-02 13:13:31 +0000

    fix: update dependencies and improve PostHog integration in the application

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,15 @@
 import "antd/dist/reset.css";
+import posthog from "posthog-js/dist/module.full";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
+
+posthog.init("phc_y32qC29aZS8xjNez6YBKH6r1EdaV6mQHDJd38j9Eiun", {
+	api_host: "https://ph.celerum.online/@",
+	ui_host: "https://eu.posthog.com",
+	defaults: "2025-11-30",
+});
+console.log("PostHog initialized");
 
 const container = document.getElementById("root");
 if (!container) throw new Error("No root element found");
